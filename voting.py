1c222e2f7d5dd590efea7db4bf194609cc1f97e6
MoonlordZI <sdannynguyen1101@outlook.com>
2022-08-04 00:24:56 +0800

    testing

--- voting.py
+++ voting.py
@@ -1,10 +1,10 @@
-
+import time
 import csv 
 
 
 with open("Sample Electoral Roll 2022.csv") as file:
     reader = csv.DictReader(file)
-    Sname = input("what is your Surname?")
+    Sname = input("what is your Surname? ")
     for row in reader:
         if row['Surname'] != Sname:
             print("your ugly ass is missing baka")
@@ -12,3 +12,10 @@ with open("Sample Electoral Roll 2022.csv") as file:
         if row['Surname'] == Sname:
             print("we found your ugly ass")
             break
+print("Welcome to today's election votes")
+time.sleep(1)
+print("Please fill in your credentials for verification ")
+time.sleep(1)
+FirstName = input("what is your First name? ")
+MiddleName = input("what is your Middle name? ")
+LastName = input("what is your Last name? ")
