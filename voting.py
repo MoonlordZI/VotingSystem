f51e7fad15404aed198fbbf278097205166815fe
MoonlordZI <sdannynguyen1101@outlook.com>
2022-08-15 19:24:27 +0800

    implemented Check Address and Check Eligibility

--- voting.py
+++ voting.py
@@ -1,3 +1,4 @@
+from ast import Global
 import time
 import csv
 import pandas as pd
@@ -8,8 +9,10 @@ print("Please fill in your credentials for verification ")
 time.sleep(1)
 
 def CheckName():
+    global df #so i can use outside this function
     df = pd.read_csv('Sample Electoral Roll 2022.csv') #opens voting csv file
 
+    global FirstName #so i can use outside this function
     FirstName = input("What is your First Name? ")
     if FirstName not in df.values: #checking if inputted Firstname exsist in csv file
         time.sleep(1)
@@ -32,14 +35,16 @@ def CheckName():
     else:
         time.sleep(1)
         print('OK')
+        time.sleep(1)
 
-    print("Virifying your data...")
+    print("Verifying your data...")
     time.sleep(1)
 
-    virify = df.loc[df['First Name'].isin([FirstName])]['Surname'].iat[0] #returns the corresponding surname of the inputted firstname 
-    if virify == Surname: #checks if the returned surname is same as the inputted surname
+    verify = df.loc[df['First Name'].isin([FirstName])]['Surname'].iat[0] #returns the corresponding surname of the inputted firstname 
+    if verify == Surname: #checks if the returned surname is same as the inputted surname
         time.sleep(1)
         print("OK")
+        time.sleep(1)
     else:
         time.sleep(1)
         print("FAIL")
@@ -48,3 +53,34 @@ def CheckName():
         exit()
 CheckName()
 
+def CheckAddress():
+
+
+    HouseNum = int(input("What is your house number? "))
+    StreetName = input("What is your street Name? ")
+    Suburb = input("What is your Suburb?")
+    Postcode = int(input('What is your Postcode? '))
+
+    C1 = df.loc[df['First Name'].isin([FirstName])]['House Number'].iat[0]
+    C2 = df.loc[df['First Name'].isin([FirstName])]['Street Name'].iat[0]
+    C3 = df.loc[df['First Name'].isin([FirstName])]['Suburb'].iat[0]
+    C4 = df.loc[df['First Name'].isin([FirstName])]['Postcode'].iat[0]
+
+    if HouseNum == C1 and StreetName == C2 and Suburb == C3 and Postcode == C4:
+        time.sleep(1)
+        print("Address Verified...")
+    else:
+        print("Invalid Address")
+        exit()
+CheckAddress()
+
+def eligibility():
+    time.sleep(1)
+    print("Checking Voting Eligibility...")
+    time.sleep(1)
+    pos = df.loc[df['First Name'].isin([FirstName])]['Has Voted?'].iat[0]
+    if pos == "No":
+        print("You are Eligible")
+    else:
+        print("You have already voted")
+eligibility()
